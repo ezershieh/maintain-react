223d54ff740e06f53f2a024ea7b69fe0c67a68d2
EzerXie <597773658@qq.com>
2018-02-01 14:23:59 +0800

    xzx:公司接口

--- src/utils/RecordsAPI.js
+++ src/utils/RecordsAPI.js
@@ -3,3 +3,5 @@ import $ from 'jquery';
 const api = "http://10.100.1.217:8080";
 
 export const getProjects = (data) => $.post(`${api}/project/select`, data)
+
+export const getProjectsCompany = (data) => $.post(`${api}/project_company/select`, data)
